b024c2df41f18b48d52e8a07f5073493432484d3
David Moore <hey@iamdavidmoore.com>
2018-05-19 22:27:10 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,9 +12,9 @@ class Artist
 
   @@artists = []
 
-  # def self.find_by_name(name)
-  #   @@artists.detect{|a| a.name == name}
-  # end
+  def self.find_by_name(name)
+    super
+  end
 
   def initialize
     super
